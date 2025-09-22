b3bbe6e6248d23f3afac9b0fd7bc5ec3843f823e
Viktoriia Vykhrystiuk bc2025-2 <viktoriavykhrystiuk@gmail.com>
2025-09-22 23:00:59 +0300

    Added Node.js program

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 
+const { URL, URLSearchParams } = require('url');
+
+const date = "202202";
+
+const baseUrl = "https://bank.gov.ua/NBU_Exchange/exchange_site";
+
+const url = new URL(baseUrl);
+
+url.search = new URLSearchParams({
+    date: date,
+    json: ""
+});
+
+console.log(url.toString());
+
