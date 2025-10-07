678d437b690608aaeb884007eca40b01c3867a79
viktoriiavykhrystiuk <viktoriiavykhrystiuk@gmail.com>
2025-10-07 09:18:10 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-const https = require('https');
-const fs = require('fs');
 const { URL, URLSearchParams } = require('url');
 
-const date = "20220201";
+const date = "202202"; // лютий 2022
 
-const nbuBaseUrl = new URL("https://bank.gov.ua/");
+const nbuBaseUrl = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/reserves");
 
 const params = new URLSearchParams({
   date: date,
   json: ""
 });
 
-nbuBaseUrl.search = params;
+nbuBaseUrl.search = params.toString();
 
 console.log(nbuBaseUrl.toString());
 
