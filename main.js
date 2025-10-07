477b4a440f9c8d17d074769b95a7655eaa20f602
viktoriiavykhrystiuk <viktoriiavykhrystiuk@gmail.com>
2025-10-07 09:13:40 +0300

    Update main.js
    виправила посилання

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-
+const https = require('https');
+const fs = require('fs');
 const { URL, URLSearchParams } = require('url');
 
 const date = "20220201";
 
-const baseUrl = "https://bank.gov.ua/NBU_Exchange/exchange_site";
+const nbuBaseUrl = new URL("https://bank.gov.ua/");
 
-const url = new URL(baseUrl);
-
-url.search = new URLSearchParams({
-    date: date,
-    json: ""
+const params = new URLSearchParams({
+  date: date,
+  json: ""
 });
 
-console.log(url.toString());
+nbuBaseUrl.search = params;
+
+console.log(nbuBaseUrl.toString());
 
